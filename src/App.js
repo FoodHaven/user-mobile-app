71bc65a40e0ad273a854bc2c8bbabfcbab885647
Ziming Mao <ziming.mao@yale.edu>
2020-10-03 13:49:17 -0400

    .

--- src/App.js
+++ src/App.js
@@ -6,38 +6,19 @@ import {
   Switch,
   Redirect,
 } from "react-router-dom";
-// Styles
 import "./styles/tailwind.css";
-// import { AnimatedSwitch } from "react-router-transition";
-
-// Screens
-import Main from "./screens/main";
-import Checkout from "./screens/checkout";
-import Test from "./screens/test";
-import Dashboard from "./screens/restaurant/dashboard";
-import Menu from "./screens/restaurant/menu";
-import CampaignDashboard from "./screens/campaign/campaignDashboard";
 
 import UserDashboard from "./screens/user/userDashboard";
 import AccountDashboard from "./screens/user/accountDashboard";
-import CampaignDetail from "./screens/campaign/campaignDetail";
 
 // Admin
-import Admin from "./screens/admin/admin";
+// import Admin from "./screens/admin/admin";
 
 function App({ location }) {
   return (
     <Router>
       <Switch>
-        <Route exact path="/campaign/:campaignID" component={CampaignDetail} />
-        <Route exact path="/campaign" component={CampaignDashboard} />
         <Route exact path="/deals" component={UserDashboard} />
-        <Route exact path="/admin" component={Admin} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/menu" component={Menu} />
-        <Route exact path="/test" component={Test} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route exact path="/" component={Main} />
         <Route exact path="/account" component ={AccountDashboard} />
       </Switch>{" "}
     </Router>
